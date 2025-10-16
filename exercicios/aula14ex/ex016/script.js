eb170ea1ceed7997bcdac36442debce7440fb554
FelipeTofoletti <fe.tofoletti@gmail.com>
2025-10-16 18:42:55 -0300

    ç

--- exercicios/aula14ex/ex016/script.js
+++ exercicios/aula14ex/ex016/script.js
@@ -1,1 +1,10 @@
-alert('OLA')
+function contar() {
+    let n1 = Number(document.getElementById('n1').value)
+    let n2 = Number(document.getElementById('n2').value)
+    let passo = Number(window.document.getElementById('passo').value)
+
+    do {
+        document.getElementById("contador").innerHTML = `${n1}`
+        n1++
+    } while (n1 >= n2)
+}
